8fc59976a5c8f1d83a9b06737ad096b1add2b039
lukemcke <lukemcke@y7mail.com>
2019-05-29 13:48:36 +1000

    SQL updated

--- assignment3.sql
+++ assignment3.sql
@@ -1,14 +1,13 @@
 use assignment3;
 
 DROP TABLE UserAccount;
+DROP TABLE IssueComment;
 DROP TABLE Issue;
 DROP TABLE KnowledgeBase;
-DROP TABLE IssueComment;
-DROP TABLE Category;
-DROP TABLE SubCategory;
+
 
 CREATE TABLE UserAccount (
-UserID int Primary key,
+UserID int Primary key AUTO_INCREMENT,
 FirstName VARCHAR(50),
 LastName VARCHAR(50),
 Email VARCHAR(50),
@@ -17,18 +16,21 @@ Password VARCHAR(50),
 IsAdmin Bool);
 
 CREATE TABLE Issue (
-IssueID INT Primary Key,
+IssueID INT Primary Key AUTO_INCREMENT,
 Title VARCHAR(40),
 Description VARCHAR(256),
-ResolveDetails VARCHAR(256),
+ResolveDetails VARCHAR(256) NULL,
 DateReported Datetime,
 DateResolved Datetime NULL,
 Status VARCHAR(30) CHECK(Status = "New" OR Status = "In Progress" OR Status = "Waiting on third Party" 
 OR Status = "Completed" OR Status = "Not Completed" OR Status = "Resolved"),
-CategoryID INT);
+Category VARCHAR(50),
+SubCategory VARCHAR(50),
+UserID Int,
+FOREIGN KEY (UserID) REFERENCES UserAccount(UserID));
 
 CREATE TABLE KnowledgeBase (
-ArticleID INT Primary Key,
+ArticleID INT Primary Key AUTO_INCREMENT,
 OriginalIssue VARCHAR(50),
 Description VARCHAR(256),
 ResolveDetails VARCHAR(256),
@@ -36,27 +38,19 @@ DateSolved Datetime,
 Comment VARCHAR(256));
 
 CREATE TABLE IssueComment (
-CommentID INT Primary Key,
+CommentID INT Primary Key AUTO_INCREMENT,
 Title VARCHAR(50),
 Field VARCHAR(256),
 IssueID INT,
 FOREIGN KEY (IssueID) REFERENCES Issue(IssueID));
-
-CREATE TABLE Category (
-CategoryID INT Primary Key,
-CategoryType VARCHAR(50),
-SubCategoryID INT,
-FOREIGN KEY (CategoryID) REFERENCES Issue(CategoryID)
-)
-
-CREATE TABLE SubCategory(
-SubCategoryID INT Primary Key,
-SubCategoryType VARCHAR(50),
-FOREIGN KEY (SubCategoryID) REFERENCES Category(SubCategoryID)
-);
 
 insert into UserAccount values (0001, "James", "Smith", "james@gmail.com", 0445795767, "password123", true);
 insert into UserAccount values (0002, "David", "Smithy", "david@gmail.com", 0400864565, "password123", false);
 insert into UserAccount values (0003, "Jane", "Smithers", "jane@gmail.com", 0449219345, "password123", false);
 insert into UserAccount values (0004, "Stacey", "Smiths", "stacey@gmail.com", 0485663232, "password123", false);
 insert into UserAccount values (0005, "Chad", "Smitts", "stacey@gmail.com", 0483321113, "password123", false);
+
+insert into Issue (Title, Description, ResolveDetails, DateReported, DateResolved, Status, Category, SubCategory)
+			Values ("Help can't connect", "Display error when connecting", null, "2019-05-29", null, "New", "Network", "Can't Connect");
+
+SELECT * FROM Issue;
